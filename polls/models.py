7e96d137af11b1c407388c0dd1cfb35afefcfe87
Trace Johnson <trace.johnson@gmail.com>
2017-04-08 20:17:04 -0400

    initial commit

---
+++ polls/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    # Return a more valid object representation of a models
+    def  __str__(self):
+        return self.question_text
+
+    # Demonstrating the purpose of the __str__ method
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # Return a more valid object representation of a models
+    def  __str__(self):
+        return self.choice_text
